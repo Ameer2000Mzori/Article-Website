69770515b3c9423ced95bc5a61d1813fbd288403
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-24 19:15:19 +0100

    added post logic to the controllers.js

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -1,24 +1,40 @@
+const articlesData = [
+  {
+    user: 'ameer',
+    title: 'there was a man...',
+    story:
+      'there was a man called ameer, he wanted to become a full stack developer',
+  },
+  {
+    user: 'ameer mzori',
+    title: 'story about me',
+    story: 'hi im ameer mzori',
+  },
+  {
+    user: 'ameer',
+    title: 'about my brother',
+    story: 'hi im ameer and my brother called atro or ameen',
+  },
+]
+
 export const homePage = (req, res) => {
   res.send('Hello World!')
 }
 
 export const ArticlesPage = (req, res) => {
-  res.send([
-    {
-      user: 'ameer',
-      title: 'there was a man...',
-      story:
-        'there was a man called ameer, he wanted to become a full stack developer',
-    },
-    {
-      user: 'ameer mzori',
-      title: 'story about me',
-      story: 'hi im ameer mzori',
-    },
-    {
-      user: 'ameer',
-      title: 'about my brother',
-      story: 'hi im ameer and my brother called atro or ameen',
-    },
-  ])
+  res.send(articlesData)
+}
+
+export const createArticle = (req, res) => {
+  const { user, title, story } = req.body
+
+  const newArticle = {
+    user,
+    title,
+    story,
+  }
+
+  articlesData.push(newArticle)
+  console.log('our new article:', newArticle)
+  console.log('our  articles:', articlesData)
 }
--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -1,11 +1,17 @@
 // importing
 import express from 'express'
-import { homePage, ArticlesPage } from '../controllers/controller.js'
+import {
+  homePage,
+  ArticlesPage,
+  createArticle,
+} from '../controllers/controller.js'
 
 const router = express.Router()
 
 router.get('/', homePage)
 
-router.get('/Articles', ArticlesPage)
+router.get('/articles', ArticlesPage)
+
+router.post('/articles/create', createArticle)
 
 export default router
