33972add896cbc3329aa804efb3a056178356332
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-25 17:04:02 +0100

    added logic where you fetch data from mongodb

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -1,28 +1,37 @@
-let articlesData = [
-  {
-    user: 'ameer',
-    title: 'there was a man...',
-    story:
-      'there was a man called ameer, he wanted to become a full stack developer',
-  },
-  {
-    user: 'ameer mzori',
-    title: 'story about me',
-    story: 'hi im ameer mzori',
-  },
-  {
-    user: 'ameer',
-    title: 'about my brother',
-    story: 'hi im ameer and my brother called atro or ameen',
-  },
-]
+import { article } from '../models/articles.js'
+
+// let articlesData = [
+//   {
+//     user: 'ameer',
+//     title: 'there was a man...',
+//     story:
+//       'there was a man called ameer, he wanted to become a full stack developer',
+//   },
+//   {
+//     user: 'ameer mzori',
+//     title: 'story about me',
+//     story: 'hi im ameer mzori',
+//   },
+//   {
+//     user: 'ameer',
+//     title: 'about my brother',
+//     story: 'hi im ameer and my brother called atro or ameen',
+//   },
+// ]
 
 export const homePage = (req, res) => {
   res.send('Hello World! Articles')
 }
 
 export const ArticlesPage = (req, res) => {
-  res.send(articlesData)
+  article
+    .find()
+    .sort({
+      createAt: -1,
+    })
+    .then((result) => {
+      res.status(200).json(result)
+    })
 }
 
 export const createArticle = (req, res) => {
---
+++ backend/src/models/articles.js
@@ -0,0 +1,24 @@
+import { Timestamp } from 'mongodb'
+import mongoose, { Schema } from 'mongoose'
+
+const articleSchema = new Schema(
+  {
+    user: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    story: {
+      type: String,
+      required: true,
+    },
+  },
+  { Timestamp: true }
+)
+
+const article = mongoose.model('article', articleSchema)
+
+export { article }
